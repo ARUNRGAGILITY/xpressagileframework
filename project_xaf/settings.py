6dc7d74ab7872f14343d8cf537d7432cb89421b1
Arun R G <arunrg.agility@gmail.com>
2023-04-07 18:05:46 +0530

    Initial commit

--- project_xaf/settings.py
+++ project_xaf/settings.py
@@ -7,7 +7,8 @@ DATABASE_NAME = os.environ.get('DATABASE_NAME')
 DATABASE_USER = os.environ.get('DATABASE_USER')
 DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 DATABASE_HOST = os.environ.get('DATABASE_HOST')
-DATABASE_PORT = os.environ.get('DATABASE_PORT')
+DATABASE_PORT = '5432'
+#os.environ.get('DATABASE_PORT')
 
 
 
